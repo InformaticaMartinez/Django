f6f08ae4036ca3b901583f50243b53807614d152
InformaticaMartinez <julionicolasmartinez@gmail.com>
2025-11-07 20:52:17 -0300

    Templates

--- src/django/aulas/urls.py
+++ src/django/aulas/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from .views import hello,bye,edad
-
+from .views import primer_plantilla,segunda_plantilla,tercer_plantilla
 
 urlpatterns = [
     path('hello', hello),
     path('bye', bye),
-    path('edad/<int:anios>/<int:futuro>', edad)
+    path('edad/<int:anios>/<int:futuro>', edad),
+    path('plantilla1', primer_plantilla),
+    path('plantilla2', segunda_plantilla),
+    path('plantilla3', tercer_plantilla),
 ]
--- src/django/aulas/views.py
+++ src/django/aulas/views.py
@@ -1,8 +1,9 @@
 #from django.shortcuts import render
 from django.http import HttpResponse  
 from datetime import date
-
-
+from django.template import Template,Context
+from django.template.loader import get_template
+from django.shortcuts import render
 def hello(request):
     return HttpResponse("Hello, world!")
 
@@ -16,4 +17,37 @@ def edad(request,anios,futuro):
     mensaje = "En el año %d cumpliras %d años" % (futuro,cumplira)
 
     return HttpResponse(mensaje)
+
+def primer_plantilla(request):
+
+   plantilla = """
+   <html>
+       <body>
+           <h1>Hola {{nombre}}!Esta es mi primera plantilla en archivo</h1>
+       </body>
+   </html>
+    """ 
+   tpl = Template(plantilla)
+   ctx = Context({
+         "nombre":"Juan"
+   })
     
+   mensaje = tpl.render(ctx)
+
+   return HttpResponse(mensaje)
+
+def segunda_plantilla(request):
+
+    tpl = get_template('segunda_plantilla.html')
+    mensaje = tpl.render({
+        "nombre":"Pedro",
+        "fecha_actual":date.today()
+    })
+    return HttpResponse(mensaje)
+
+def tercer_plantilla(request):
+
+   return render(request,'tercer_plantilla.html',{
+       "nombre":"Julio Martinez",
+       "fecha_actual":date.today()
+   })
